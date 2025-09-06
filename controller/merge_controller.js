822877dd0b94c886d486522e9cd88aa7b2fd8c8c
prajapai-satyam <satyamprajapati1979@gmail.com>
2025-09-06 11:41:23 +0530

    --add merge pdf functionlity

---
+++ controller/merge_controller.js
@@ -0,0 +1,68 @@
+import upload from "../middleware/multer.js"
+import { merge_pdf } from "../utils/split_merge_pdf.js";
+import fs from 'fs'
+
+const merge_pdf_controller = async (req,res) => {
+upload.array('test')(req,res, async(err) => {
+if (err) {
+      if (err.code === "LIMIT_FILE_SIZE") {
+        return res.status(400).json({
+          message: "Maximum allowed file size is 5MB",
+        });
+      }
+      if (err.code === "LIMIT_UNEXPECTED_FILE") {
+        return res.status(400).json({
+          message: "You sent wrong field name with PDF file, check and try again",
+        });
+      }
+      return res.status(400).json({ message: err.message });
+    }
+
+    if (!req.files || req.files.length < 2) {
+      return res.status(400).json({ message: "Minimum 2 pdf files are required for merge it!" });
+    }
+    let req_path_path = [];
+    req.files.forEach((file) => {
+        req_path_path.push(file.path)
+    });
+    console.log(req_path_path);
+    try {
+        const final_merge_pdf_path = await merge_pdf([...req_path_path]);
+        if (final_merge_pdf_path.path && final_merge_pdf_path.success === true) {
+             return res.status(200).download(final_merge_pdf_path.path, 'merge.pdf', (err) => {
+                // console.log("file deleted start");
+                fs.unlinkSync(final_merge_pdf_path.path);
+                // console.log("file deleted end ");
+                req.files.forEach((file) => {
+                fs.unlinkSync(file.path);
+            })
+             })
+        }
+        else {
+            if (fs.existsSync(final_merge_pdf_path.path)) {
+                fs.unlinkSync(final_merge_pdf_path.path);
+                console.log("tempory created file deleted");
+            }
+            req.files.forEach((file) => {
+                fs.unlinkSync(file.path);
+            })
+            return res.status(400).json({
+                message: "uanble to merge you pdf , try again"
+            })
+        }
+
+    } catch (err) {
+        if (fs.existsSync(final_merge_pdf_path.path)) {
+                fs.unlinkSync(final_merge_pdf_path.path);
+                console.log("tempory created file deleted");
+            }
+        console.log("error in merge file controler : ", err);
+        return res.status(400).json({
+            message: "something went wrong, make sure you send pdf files and they are not encrpted"
+        })
+
+    }
+})
+}
+
+export default merge_pdf_controller
---
+++ utils/split_merge_pdf.js
@@ -0,0 +1,134 @@
+import fs, { mkdtempSync } from "fs";
+import path from "path";
+import { PDFDocument } from "pdf-lib";
+import { zip, COMPRESSION_LEVEL } from 'zip-a-folder';
+
+
+async function split_pdf(inputpath, range, pagenumbers, allpagesplit) {
+  if (!inputpath || !(fs.existsSync(inputpath))) {
+    console.log("input path error")
+    return false;
+  }
+  if ([range, pagenumbers, allpagesplit].filter(Boolean).length >= 2) {
+    console.log("only one parameter can be execute at once")
+    process.exit(1);
+  }
+  const existingPdfBytes = fs.readFileSync(inputpath);
+
+  // for if user give all page split true
+  if (allpagesplit === true) {
+    const existingPdf = await PDFDocument.load(existingPdfBytes);
+    // const newPdf = await PDFDocument.create();
+    const page_count = existingPdf.getPageIndices();
+    const random_dir = fs.mkdtempSync("hello split");
+
+    for (let i = 0; i < page_count.length; i++) {
+      const singlePagePdf = await PDFDocument.create();
+      const [page] = await singlePagePdf.copyPages(existingPdf, [i]);
+      singlePagePdf.addPage(page);
+      const pdfBytes = await singlePagePdf.save();
+      fs.writeFileSync(`./${random_dir}/page_${i + 1}.pdf`, pdfBytes);
+    }
+
+    const zip_file = await zip(`./${random_dir}`, `./${random_dir}.zip`, { compression: COMPRESSION_LEVEL.high });
+    fs.rmSync(random_dir, { recursive: true, force: true });
+    return {path: path.resolve(`${random_dir}.zip`), success: true}
+  }
+
+  // for if user give range of two numbers
+  if (range && range.length === 2 && range[0] < range[1] && range[1] > range[0]) {
+    let page_count2 = [];
+    while (range[0] <= range[1]) {
+      page_count2.push(range[0]);
+      range[0]++;
+    }
+    // console.log("i am page count ", page_count2)
+    const existingPdf = await PDFDocument.load(existingPdfBytes);
+
+
+    const newPdf = await PDFDocument.create();
+
+    const random_dir = fs.mkdtempSync("hello split");
+
+for (let i = 0; i < page_count2.length; i++) {
+      const singlePagePdf = await PDFDocument.create();
+      const [page] = await singlePagePdf.copyPages(existingPdf, [i]);
+      singlePagePdf.addPage(page);
+      const pdfBytes = await singlePagePdf.save();
+      fs.writeFileSync(`./${random_dir}/page_${page_count2}.pdf`, pdfBytes);
+    }
+    await zip(`./${random_dir}`, `${random_dir}.zip`, {
+      compression: COMPRESSION_LEVEL.high
+    })
+    fs.rmSync(random_dir, {recursive: true, force: true});
+    return {path: path.resolve(`${random_dir}.zip`), success: true}
+  }
+// for is user give random page numbers
+  if (pagenumbers && pagenumbers.length > 0) {
+    const existingPdf = await PDFDocument.load(existingPdfBytes);
+
+
+    const newPdf = await PDFDocument.create();
+    const random_dir = mkdtempSync("hello split");
+for (let i = 0; i < pagenumbers.length; i++) {
+  const pageIndex = pagenumbers[i] - 1;
+
+  const singlePagePdf = await PDFDocument.create();
+  const [page] = await singlePagePdf.copyPages(existingPdf, [pageIndex]);
+  singlePagePdf.addPage(page);
+
+  const pdfBytes = await singlePagePdf.save();
+
+  // keep the file name same as user input (1-based)
+  fs.writeFileSync(`${random_dir}/page_${pagenumbers[i]}.pdf`, pdfBytes);
+}
+await zip(`./${random_dir}`, `${random_dir}.zip`, {
+  compression: COMPRESSION_LEVEL.high
+})
+fs.rmSync(random_dir, {recursive: true, force:true})
+return {path: path.resolve(`${random_dir}.zip`), success: true}
+  }
+}
+
+// function for merge pdf
+
+async function merge_pdf(pdfpaths) {
+ if (pdfpaths.length < 2) {
+     console.log("minimum two pdf file path required to merge it");
+     process.exit(1);
+ }
+ let pdfBytesLoad = [];
+ // buffer data of pdf
+for (let i = 0; i < pdfpaths.length; i++) {
+  const existingPdfBytes = fs.readFileSync(pdfpaths[i]);
+  pdfBytesLoad.push(existingPdfBytes);
+}
+console.log("length of pdf bytes load : ",pdfBytesLoad.length);
+// console.log("data of pdf bytes load : ", pdfBytesLoad);
+let existingPdf = [];
+// load data in pdf document
+for (let i = 0; i < pdfBytesLoad.length; i++) {
+   const loadpdf = await PDFDocument.load(pdfBytesLoad[i]);
+   existingPdf.push(loadpdf);
+}
+console.log("length of existing pdf : ", existingPdf.length);
+// console.log("existing pdf data : ", existingPdf);
+
+// making final pdf
+// let bytesOfFinalPdf = [];
+const final_pdf = await PDFDocument.create();
+for (let i = 0; i < existingPdf.length; i++) {
+const copyPages = await final_pdf.copyPages(existingPdf[i], existingPdf[i].getPageIndices());
+
+copyPages.forEach((page) => {
+  final_pdf.addPage(page)
+});
+}
+const finalPdfBytes = await final_pdf.save();
+const random_number = Math.floor(100000 + Math.random() * 900000);
+fs.writeFileSync(`merge_${random_number}.pdf`, finalPdfBytes);
+return {path: path.resolve(`merge_${random_number}.pdf`), success: true}
+}
+
+
+export {split_pdf, merge_pdf}
